ff211bc0dd782a4ef0e3e0cc1377b1bf739206f7
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 14:55:22 -0300

    Create interfaces for days of stock data classes

---
+++ CsvImporter/CsvImporter/Models/IDayStock.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CsvImporter.Models
+{
+    /// <summary>
+    /// Defines the methods for a day stock
+    /// </summary>
+    public interface IDayStock
+    {
+        /// <summary>
+        /// Identifier for the stock
+        /// </summary>
+        public StockId StockId { get; }
+    }
+}
---
+++ CsvImporter/CsvImporter/Models/IMultipleDaysStock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Defines a grouper for days stock, as comparable
+/// </summary>
+namespace CsvImporter.Models
+{
+    interface IMultipleDaysStock : IDayStock, IComparable<MultipleDaysStock>
+    {
+    }
+}
