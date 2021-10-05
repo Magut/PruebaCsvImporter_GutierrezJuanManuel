dff8f996d6fc3d8f1c7c873148644b45bd087f4c
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 15:01:12 -0300

    Create IStockWriter
    Defines the methods for an Imported Stock Data Writer

---
+++ CsvImporter/CsvImporter/Controllers/Writers/IStockWriter.cs
@@ -0,0 +1,28 @@
+﻿using CsvImporter.Models;
+using System.Threading.Tasks;
+
+namespace CsvImporter.Controllers.Writers
+{
+    /// <summary>
+    /// Defines the methods for an Imported Stock Data Writer
+    /// </summary>
+    interface IStockWriter
+    {
+        /// <summary>
+        /// It starts writing in the target the data located in the input queue
+        /// </summary>
+        /// <returns>No object or value is returned by this method when it completes</returns>
+        public Task StartWritingDataAsync();
+        /// <summary>
+        /// Insert asynchrounously row/s in the target
+        /// </summary>
+        /// <param name="data">Row/s with the data</param>
+        /// <returns>No object or value is returned by this method when it completes</returns>
+        public Task InsertAsync(IDayStock data);
+        /// <summary>
+        /// Deletes all stock rows asynchrounously in the target
+        /// </summary>
+        /// <returns>No object or value is returned by this method when it completes</returns>
+        public Task DeleteAllAsync();
+    }
+}
