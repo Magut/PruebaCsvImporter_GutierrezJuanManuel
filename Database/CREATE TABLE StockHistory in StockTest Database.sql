d010a3d8c5fcbb177ffc93b60d27cc88e0f0d14e
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 14:48:35 -0300

    Create Databe and Table for testing

---
+++ Database/CREATE TABLE StockHistory in StockTest Database.sql
@@ -0,0 +1,19 @@
+USE [StockTest]
+GO
+
+/****** Object:  Table [dbo].[StockHistory]    Script Date: 2/10/2021 11:08:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[StockHistory](
+	[PointOfSale] [int] NOT NULL,
+	[Product] [int] NOT NULL,
+	[Date] [date] NOT NULL,
+	[Stock] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+
