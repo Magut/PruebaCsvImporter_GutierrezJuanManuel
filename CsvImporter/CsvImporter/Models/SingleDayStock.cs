bb6edd327bb6178cc3d05eb579d1b0518328bf99
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 15:09:27 -0300

    Create SingleDayStock
    Models a single day stock

---
+++ CsvImporter/CsvImporter/Models/SingleDayStock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace CsvImporter.Models
+{
+    /// <summary>
+    /// Models a single day stock
+    /// </summary>
+    public class SingleDayStock : IDayStock
+    {
+        private readonly StockId _stockId;
+        private readonly DateTime _date;
+
+        /// <summary>
+        /// Constructor with all the needed data
+        /// </summary>
+        /// <param name="stockId"><see cref="StockId"/> with the initialized data of the stock</param>
+        /// <param name="date"></param>
+        public SingleDayStock(StockId stockId, DateTime date)
+        {
+            _stockId = stockId;
+            _date = date.Date;
+        }
+
+        /// <summary>
+        /// Constructor with all the needed data in a <see cref="String"/>
+        /// with the format of a CSV row
+        /// </summary>
+        /// <param name="csvRow">CSV row</param>
+        public SingleDayStock(string csvRow, char separator = ';')
+        {
+            string[] csvRowFields = csvRow.Split(separator);
+            if (!long.TryParse(csvRowFields[0], out long pointOfSale))
+                throw new ArgumentException("The parameter is not a valid CSV row", "csvRow");
+            if (!long.TryParse(csvRowFields[1], out long product))
+                throw new ArgumentException("The parameter is not a valid CSV row", "csvRow");
+            if (!DateTime.TryParseExact(csvRowFields[2], "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+                throw new ArgumentException("The parameter is not a valid CSV row", "csvRow");
+            if (!long.TryParse(csvRowFields[3], out long stock))
+                throw new ArgumentException("The parameter is not a valid CSV row", "csvRow");
+
+            _stockId = new StockId(pointOfSale, product, stock);
+            _date = date;
+        }
+
+        public StockId StockId => _stockId;
+        public long PointOfSale => _stockId.PointOfSale;
+        public long Product => _stockId.Product;
+        public long Stock => _stockId.Stock;
+        public DateTime Date => _date;
+    }
+}
