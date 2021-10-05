d4789901f119704a9ce257945d78aeb86db84dfd
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 15:00:50 -0300

    Create ICsvDataProcessor
    Defines needed methods for a CSV Data Processor

---
+++ CsvImporter/CsvImporter/Controllers/Processors/ICsvDataProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace CsvImporter.Controllers.Processors
+{
+    /// <summary>
+    /// Defines needed methods for a CSV Data Processor
+    /// </summary>
+    interface ICsvDataProcessor
+    {
+        /// <summary>
+        /// It starts processing the CSV rows enqueued by the reader
+        /// and enqueues the data read in another queue to feed the writer
+        /// </summary>
+        /// <returns>No object or value is returned by this method when it completes</returns>
+        public Task StartProcessingAndEnqueuingDataAsync();
+    }
+}
