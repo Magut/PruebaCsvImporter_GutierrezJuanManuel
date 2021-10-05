19d756c4b287cf50b8396c2b0a7fd9422002702a
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 14:56:00 -0300

    Create Flag
    Used to pass as reference a flag event to an object initializer (or other similar applications)

---
+++ CsvImporter/CsvImporter/Models/Flag.cs
@@ -0,0 +1,12 @@
+﻿namespace CsvImporter.Models
+{
+    public class Flag
+    {
+        private bool _event = false;
+        public bool Event
+        {
+            get => _event;
+            set => _event = value;
+        }
+    }
+}
