69c5100269fa2453b1e4fea5274fe0729e172f1e
Magut <jmgutierrez.ar@gmail.com>
2021-10-05 14:58:41 -0300

    Create interfaces for data reading (from files and streams)

---
+++ CsvImporter/CsvImporter/Controllers/Readers/IDataReader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CsvImporter.Controllers.Readers
+{
+    /// <summary>
+    /// Defines needed methods for a data reader
+    /// </summary>
+    interface IDataReader
+    {
+        /// <summary>
+        /// It starts reading until the end
+        /// and equeues the rows read in a queue
+        /// </summary>
+        /// <returns>No object or value is returned by this method when it completes</returns>
+        public Task StartReadingAndEqueuingDataAsync();
+    }
+}
---
+++ CsvImporter/CsvImporter/Controllers/Readers/IFileReader.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace CsvImporter.Controllers.Readers
+{
+    /// <summary>
+    /// Defines needed methods for a file reader
+    /// </summary>
+    interface IFileReader
+    {
+        /// <summary>
+        /// It starts reading the file until the end
+        /// and equeues the rows read in a queue setted
+        /// </summary>
+        /// <returns>No object or value is returned by this method when it completes</returns>
+        public Task ReadFileAndEnqueueDataAsync();
+    }
+}
