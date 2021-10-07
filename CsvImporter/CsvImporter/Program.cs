26f73182f44c446763347557486c7b5249675e37
Magut <jmgutierrez.ar@gmail.com>
2021-10-07 08:55:16 -0300

    Delete logic that now belongs to Manager class

--- CsvImporter/CsvImporter/Program.cs
+++ CsvImporter/CsvImporter/Program.cs
@@ -49,49 +49,7 @@ namespace CsvImporter
 
             InputFileLocation fileLocation = location.ToLower() == "azure" ? InputFileLocation.Azure : InputFileLocation.Localhost;
 
-            if (fileLocation == InputFileLocation.Azure)
-            {
-                CheckValidUrl(path);
-            }
-            else
-            {
-                path = CheckValidLocalPath(path);
-            }
-
             return (fileLocation, path);
-        }
-
-        /// <summary>
-        /// Checks if the parameter <paramref name="url"/> looks like a valid URL
-        /// </summary>
-        /// <param name="url">URL</param>
-        /// <returns><see langword="true"/> if the <paramref name="url"/> looks like a valid URL, else <see langword="false"/></returns>
-        private static bool CheckValidUrl(string url)
-        {
-            Uri uriResult;
-            return Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-        }
-
-        /// <summary>
-        /// Checks if the parameter <paramref name="path"/> looks like a valid Path
-        /// </summary>
-        /// <param name="path">Path</param>
-        /// <returns><see langword="false"/> if the <paramref name="path"/> looks like a valid Path, else <see langword="false"/></returns>
-        private static string CheckValidLocalPath(string path)
-        {
-            string fullPath;
-
-            try
-            {
-                fullPath = Path.GetFullPath(path);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Please enter a valid file path.");
-                throw new ArgumentException("The argument file path passed to the process is not valid");
-            }
-
-            return fullPath;
         }
     }
 
