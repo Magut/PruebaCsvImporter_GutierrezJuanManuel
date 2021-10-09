3ad01649b9b30f3876e3a88393ebb94eb9ac842a
Magut <jmgutierrez.ar@gmail.com>
2021-10-08 21:06:55 -0300

    Change data type

--- CsvImporter/CsvImporter/Models/SingleDayStock.cs
+++ CsvImporter/CsvImporter/Models/SingleDayStock.cs
@@ -31,13 +31,11 @@ namespace CsvImporter.Models
         public SingleDayStock(string csvRow, char separator = ';')
         {
             string[] csvRowFields = csvRow.Split(separator);
-            if (!long.TryParse(csvRowFields[0], out long pointOfSale))
-                throw new ArgumentException("The parameter is not a valid CSV row", nameof(csvRow));
-            if (!long.TryParse(csvRowFields[1], out long product))
-                throw new ArgumentException("The parameter is not a valid CSV row", nameof(csvRow));
+            string pointOfSale = csvRowFields[0];
+            string product = csvRowFields[1];
             if (!DateTime.TryParseExact(csvRowFields[2], "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
                 throw new ArgumentException("The parameter is not a valid CSV row", nameof(csvRow));
-            if (!long.TryParse(csvRowFields[3], out long stock))
+            if (!int.TryParse(csvRowFields[3], out int stock))
                 throw new ArgumentException("The parameter is not a valid CSV row", nameof(csvRow));
 
             _stockId = new StockId(pointOfSale, product, stock);
