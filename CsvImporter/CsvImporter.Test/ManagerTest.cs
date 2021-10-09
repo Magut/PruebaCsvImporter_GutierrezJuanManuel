062fc2e974edee848f755443db083ea6ca531f17
Magut <jmgutierrez.ar@gmail.com>
2021-10-08 21:12:26 -0300

    Create ManagerTest

---
+++ CsvImporter/CsvImporter.Test/ManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using CsvImporter.Controllers;
+using System;
+using Xunit;
+
+namespace CsvImporter.Test
+{
+    public class ManagerTest
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ManagerCtor_WithInvalidFilePath_ThrowsException(string filePath)
+        {
+            InputFileLocation inputFileLocation = InputFileLocation.Localhost;
+
+            Assert.Throws<ArgumentException>(() => new Manager(inputFileLocation, filePath));
+        }
+
+        [Fact]
+        public void ManagerCtor_WithValidArguments_CreatesInstance()
+        {
+            string filePath = "C:/KindOfValidPath";
+            InputFileLocation inputFileLocation = InputFileLocation.Localhost;
+
+            Manager manager = new Manager(inputFileLocation, filePath);
+
+            Assert.NotNull(manager);
+        }
+    }
+}
